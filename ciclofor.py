eac974c2389632032e2a1472b28e19cc362b69e9
Matías Sebastián Quezada Radovcic <145939113+Matias-QR@users.noreply.github.com>
2023-09-24 20:58:12 -0300

    for cycle example

---
+++ ciclofor.py
@@ -0,0 +1,49 @@
+#Ciclo FOR
+#1° Forma for
+#for w in range():-->(dato) cantidad de veces que se repite          -->para w en el rango:
+
+"""for w in range(5):
+    print(w)"""
+
+#for w in range(5):
+#   print(w)
+# w=w+1--> contador del ciclo for que no vemos
+
+#w=0
+# w=1
+# w=2            ("2 es menor que 5?...sí (verdadero)
+# w=3
+# w=4
+#                 ("5 es menor que 5?...no (falso), se detiene
+
+# 2° forma del for
+#for q in range(vi,vf): vi=valor inicial vf=valor final
+
+#mostrar los 10 primeros numeros impares.
+"""for q in range(1,11):
+    print(q*2-1,end="-")"""
+
+#3° forma del for
+#for r in range(vi,vf.i):-->i= incremento varia el valor del contador
+
+"""for r in range(5,51,5):
+    print(r,end="-")"""
+
+#Ciclo While--> mientras la condicion sea verdadera
+
+#validar un numero entero
+#ve=0 --> variable de entrada
+#While(ve<1):
+#  codigo1   (identacion)
+#  codigo2
+#codigo3
+w=0
+while(w<1):
+    w=int(input("ingrese un numero"))
+    #w=-8
+print("gracias totales")
+
+
+
+
+    
