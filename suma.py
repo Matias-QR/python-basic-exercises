44d63e4f7cf1fac4743a5b69715ead2e68a9ece8
Matías Sebastián Quezada Radovcic <145939113+Matias-QR@users.noreply.github.com>
2023-09-24 21:01:38 -0300

    Sum example

---
+++ suma.py
@@ -0,0 +1,22 @@
+#suma de dos numeros
+print("suma de dos numeros")
+#metodo carretero
+nombre=input("ingrese su nombre: ")
+print("ingrese 1° numero: ")
+#INPUT por defecto guarda variables de tipo texto caracter/string
+#Las variables y constantes NO SE DEFINEN en python, SE ASIGNAN
+
+n1=float(input())
+#metodo rapido
+n2=float(input("ingrese 2° numero: "))
+
+suma=n1+n2
+
+print("Su nombre es: ",nombre)
+print("la suma es:",suma)
+
+
+"""x=5
+w=12.4
+q="Juan Lopez"
+"""
