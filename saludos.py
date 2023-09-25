d75737323ebf3c932647adee1d06268abc938bac
Matías Sebastián Quezada Radovcic <145939113+Matias-QR@users.noreply.github.com>
2023-09-24 21:00:51 -0300

    Add files via upload

---
+++ saludos.py
@@ -0,0 +1,15 @@
+#comentario linea
+"""comentario
+de varias
+lineas"""
+
+#escribir --> print (mostrar mensaje al usuario)
+#leer --> input (capturar datos)
+
+"""tipos de datos python
+entero --> int(integer)
+caracter --> str(string)
+reales --> float
+logico --> True - False"""
+
+print("aloja a python")
